6a6313ff2dc4126ccab00d5149708fea9eb423b8
Haroun04 <Harounzemrani4@gmail.com>
2024-04-03 19:28:35 +0200

    comentacio_out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { FormsModule } from '@angular/forms'; 
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RestaurantComponent } from './restaurant/restaurant.component';
@@ -21,7 +21,7 @@ import { SearchResultsComponent } from './search-results/search-results.componen
     HttpClientModule,
     AppRoutingModule,
     RouterModule,
-    FormsModule // Agrega FormsModule aquí
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
